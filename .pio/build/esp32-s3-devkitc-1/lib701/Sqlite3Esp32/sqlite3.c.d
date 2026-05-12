fe777fcbd004f08c00cad9756acaf124216087aa
Eber Trejo <Ebersanchezt@gmail.com>
2026-05-12 09:02:04 -0400

    Remove

--- .pio/build/esp32-s3-devkitc-1/lib701/Sqlite3Esp32/sqlite3.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib701/Sqlite3Esp32/sqlite3.c.o: \
- .pio/libdeps/esp32-s3-devkitc-1/Sqlite3Esp32/src/sqlite3.c \
- .pio/libdeps/esp32-s3-devkitc-1/Sqlite3Esp32/src/config_ext.h \
- C:/Users/ebers/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32s3/include/newlib/platform_include/time.h
